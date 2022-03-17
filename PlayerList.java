f21902da21665af8c9d37f8ab7917ae7edd0d4bb
angel silaban <magdalenangela20@gmail.com>
2022-03-17 12:48:19 +0700

    Kumpul Jurnal04

---
+++ PlayerList.java
@@ -0,0 +1,20 @@
+package jejeeel;
+
+public class PlayerList {
+        private String judulLagu;
+        private String namaPenyanyi;
+
+        public PlayerList(String judulLagu, String namaPenyanyi){
+            this.judulLagu = judulLagu;
+            this.namaPenyanyi = namaPenyanyi;
+        }
+
+        public String getJudulLagu(){
+            return this.judulLagu;
+        }
+
+        @Override
+        public String toString() {
+            return this.judulLagu;
+        }
+}
